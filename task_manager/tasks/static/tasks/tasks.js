ce0202eddd496985276346922ecc5b50a37e7865
Diego Rojas <ale.rgdap@gmail.com>
2024-03-17 11:18:17 -0600

    changed to jquery

--- task_manager/tasks/static/tasks/tasks.js
+++ task_manager/tasks/static/tasks/tasks.js
@@ -1,26 +1,19 @@
-document.addEventListener('DOMContentLoaded', ()=> {
-    console.log("hello world")
-    const task = document.querySelector('.task');
-    const done = document.querySelector('.done');
-    done.addEventListener('click', function(){
-        console.log('done');
-        task.style.background = 'rgb(173,255,47, 0.5)';
-        task.style.filter = 'blur(2px)'
-        task.style.animation = 'done 0.5s'
-    });
-    const newTaskForm = document.querySelector('.new-task-container');
-    const newTaskButton = document.querySelector('.new-task-button');
-    newTaskButton.addEventListener('click', function(){
-        newTaskForm.style.display = 'block';
+$(document).ready(()=>{
+    let $form = $('.new-task-container');
+    console.log("READY")
+    let $button = $('.new-task-button');
+    $button.click(()=>{
+        $form.toggle();
     })
-
-    const form = document.querySelector('.new-task-form');
     let taskName;
     let taskDes;
-    form.addEventListener('submit', function(){
-        taskName = document.querySelector('#taskName')
-        taskDes = document.querySelector('#taskDes')
+    let taskContainer = $('.tasks-container')
+    let taskDiv = $('.task').clone()
+    $form.submit(()=>{
+        taskName = $('#taskName').text();
+        taskDes = $('#taskDes').text();
     })
-
-    
+    let top = $(taskDiv > '.task-top');
+    let title = $(top > '.task-title');
+    title.text(taskName);
 })
